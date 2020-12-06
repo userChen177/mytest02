1247b5b58ed59c034557722056ae0ca502c11ed7
chenjie105 <466070284@qq.com>
2020-12-06 18:20:12 +0800

    第一次修改OK提交

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld {
 	public static void main(String[] args) {
 		System.ount.println("hello world");
-		
+		test();
 	}
-}
+	
+	public void Test() {
+		System.out.prinln("aaa");
+	}
+ }
